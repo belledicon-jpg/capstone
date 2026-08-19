35f0b272bc6f7e2b8852ca9571b03d9bc61a1526
Dyad <git@dyad.sh>
2026-08-19 13:50:26 +0800

    Reverted all changes back to version eb189aee587ace394b887ea13af751ca145f8c80

--- src/config/eprovider.ts
+++
@@ -1,7 +0,0 @@
-export const EPROVIDER_CONFIG = {
-  apiBase: "https://supa.eprovider.site",
-  projectId: "5e712edc-df1d-4783-a1b6-26c8dacf0eec",
-  schema: "tenant_5e712edcdf1d4783a1b626c8dacf0eec",
-  anonKey:
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsInByb2plY3RfaWQiOiI1ZTcxMmVkYy1kZjFkLTQ3ODMtYTFiNi0yNmM4ZGFjZjBlZWMiLCJpYXQiOjE3ODcxMTQyNTUsImV4cCI6MjEwMjY5MDI1NSwiYXVkIjoiZXByb3ZpZGVyLXJlc3QiLCJpc3MiOiJlcHJvdmlkZXItY29udHJvbC1wbGFuZSJ9.sLHSgLeHfBZdO5JtOYNP_pIPOpiAgf4NC0AbfoI66Cs",
-};
