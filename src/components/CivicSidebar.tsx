39a7e29a95e388b28e2510199c04137e256ca85a
belle <belledicon@gmail.com>
2026-08-21 15:27:14 +0800

    Point Sanitation Services link to Sanitation page in sidebar

--- src/components/CivicSidebar.tsx
+++ src/components/CivicSidebar.tsx
@@ -23,7 +23,7 @@ type CivicSidebarProps = {
 
 const primaryLinks = [
   { label: "Overview", icon: LayoutDashboard, to: "/" },
-  { label: "Sanitation services", icon: Trash2, to: "/#services" },
+  { label: "Sanitation Services", icon: Trash2, to: "/sanitation" },
   { label: "Health inspections", icon: ClipboardCheck, to: "/#inspections" },
   { label: "Facilities", icon: Building2, to: "/#facilities" },
   { label: "Community reports", icon: Users, to: "/#community" },
